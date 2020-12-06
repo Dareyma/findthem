b20db37eb72967239b5cef2f36d8bd93772bee9f
Dareyma <Dareyma7@gmail.com>
2020-12-06 21:38:43 +0100

    nuevos cambios 6-12

--- findthem/src/main/java/com/spring/core/configuration/Paginas.java
+++
@@ -1,7 +0,0 @@
-package com.spring.core.configuration;
-
-public class Paginas {
-	public static String HOME = "index";
-	public static String POST = "post";
-	public static String REGISTER_VIEW ="register";
-}
---
+++ findthem/src/main/java/com/spring/core/constants/Constantes.java
@@ -0,0 +1,16 @@
+package com.spring.core.constants;
+
+public class Constantes {
+	
+	public static final String ACCOUNT_VIEW = "account";
+	public static final String EDIT_ACCOUNT_VIEW = "editaccount";
+	
+	public static final String POST_VIEW = "index";
+	public static final String POST_CREATE_VIEW = "createpost";
+	
+	public static final String REPLY_VIEW = "reply";
+	
+	public static final String LOGIN_VIEW = "login";
+	public static final String REGISTER_VIEW = "register";
+	
+}
